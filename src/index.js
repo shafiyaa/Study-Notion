21b9957966502562559ba1706718beb68bc87845
shafiya <khanshafiya1219@gmail.com>
2023-12-08 19:50:01 +0000

    add more course

--- src/index.js
+++ src/index.js
@@ -28,14 +28,12 @@ root.render(
 
 
 
-// android aur AI k 4 course  course banane hain
-
-// catalogue ka last section ka pics sahi krna h
 
 
 
 
-// course page k catalogue me New pe click krne se recently add courses dikhane hain
+
+
 
 
 
@@ -44,45 +42,12 @@ root.render(
 // logo gayab ho rha h small screeen
 // payment  service me verify payment mei cart epmty wala function nhi chl rha h
 // slice me completed wale array me koi value nhi aarhi h ... uske wajha se progress baar me koi reflection nhi ho rha h
-
-
-
-
-
-
-
-
-
-
-
-
-
-// last me krna h
 // instructor ko myCourse me time duration ko sahi krna  h
 
 
 
 
 
-
-
- 
-
-// category k model course update nhi ho rha h
-
-
-
-//  problems
-
-
-// Loading ka toast remove krna h (profile and setting API se)
-
-// navbar k catalog ko style krna h hover krne pe neeche line aani chahiye 
-
-
-
-// moving code ki style sahi krna h
-// lazy loading add krna
 
 
 
@@ -93,12 +58,3 @@ root.render(
 
 
 
-// -----------------yeh sab last mai krna h----------------
-// saare pages ko responsive krna h last mai 
-// learn useRef  and useOnCkickOutside function
-// lean calc()
-// @apply in Css?
-
-//--------------- optionals---------------
-
-// phone number wali input field me text type ho rha firefox mai agr sahi hoga toh karo
